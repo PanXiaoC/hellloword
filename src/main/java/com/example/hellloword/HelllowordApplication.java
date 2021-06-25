f6b3cac15230264662dd95914345c8cb58c22d9a
PanXiaoC <2770421762@qq.com>
2021-06-25 22:17:23 +0800

    测试

---
+++ src/main/java/com/example/hellloword/HelllowordApplication.java
@@ -0,0 +1,22 @@
+package com.example.hellloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelllowordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelllowordApplication.class, args);
+    }
+
+    @RequestMapping
+    public String hello() {
+        return "hello spring boot!";
+    }
+
+}
---
+++ src/test/java/com/example/hellloword/HelllowordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelllowordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
