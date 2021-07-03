b54de1334d570d1a49a91eded5fc82ffdd92d73d
lisujie <2818843662@qq.com>
2021-07-03 17:14:51 +0800

    修改了一下

--- src/main/java/com/example/hellloword/dao/userdao.java
+++ src/main/java/com/example/hellloword/dao/userdao.java
@@ -10,12 +10,18 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface userdao {
+//    查看密码
     @Select("select pwd from userinfo where username=#{username}")
     public String getPwd(userinfo user);
+//    查看userinfo表中的所有数据
     @Select("select * from userinfo where username=#{username}")
     public String getAll(userinfo user);
+//    修改密码
     @Update("update userinfo set pwd=#{pwd} where username=#{username}")
-    public int updatePwd(userinfo record);
-
+    public int updatePwd(userinfo user);
+//    注册
+    @Insert("insert into userinfo values(#{id},#{username}),#{pwd},#{gender},#{mobile},#{email}" +
+            ",#{create_time},#{last_login},#{isAdmin},#{disk_max_size}")
+    public String registerUserinfo(userinfo user);
 
 }
